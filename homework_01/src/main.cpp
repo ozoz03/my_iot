d5ac92f568c190434f4b68ad34d71c86f60ddea8
ozoz03 <oleg.zasymenko@gmail.com>
2026-08-08 17:29:22 +0300

    functions

--- homework_01/src/main.cpp
+++ homework_01/src/main.cpp
@@ -4,63 +4,69 @@
 #define LED 2
 #define EXT_LED 4
 
-// put function declarations here:
-int myFunction(int, int);
+const int DOT = 200;
+const int DASH = DOT * 3;
+const int ELEMENT_GAP = DOT;      // gap between dot/dash within a letter
+const int LETTER_GAP = DOT * 3;   // gap between letters
+const int WORD_GAP = DOT * 7;     // gap between words/phrases
 
 void setup() {
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   pinMode(EXT_LED, OUTPUT);
 
 }
 
-void loop() {
-  // S
-  for (int i = 0; i < 3; ++i) {
-    Serial.print(".");
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    delay(200);
-    // a pause
-    Serial.print(" ");
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(200);
-  }
-  // O
-  for (int i = 0; i < 3; ++i) {
-    Serial.print("-");
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    delay(600);
-    // a pause
-    Serial.print(" ");
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(200);
-  }
-  // S again
-  for (int i = 0; i < 3; ++i) {
-    Serial.print(".");
-    digitalWrite(LED, HIGH);
-    digitalWrite(EXT_LED, HIGH);
-    delay(200);
-    // a pause
-    Serial.print(" ");
-    digitalWrite(LED, LOW);
-    digitalWrite(EXT_LED, LOW);
-    delay(200);
-  }
-  // a pause between phrases
-  Serial.println("");
+void blink(int duration) {
+  digitalWrite(LED, HIGH);
+  digitalWrite(EXT_LED, HIGH);
+  delay(duration);
   digitalWrite(LED, LOW);
   digitalWrite(EXT_LED, LOW);
-  delay(200);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void sendDot() {
+  Serial.print(".");
+  blink(DOT);
+  delay(ELEMENT_GAP);
+}
+
+void sendDash() {
+  Serial.print("-");
+  blink(DASH);
+  delay(ELEMENT_GAP);
+}
+
+void sendLetterGap() {
+  delay(LETTER_GAP - ELEMENT_GAP);
+}
+
+void sendWordGap() {
+  Serial.println("");
+  delay(WORD_GAP);
+}
+
+void sendS() {
+  for (int i = 0; i < 3; ++i) {
+    sendDot();
+  }
+}
+
+void sendO() {
+  for (int i = 0; i < 3; ++i) {
+    sendDash();
+  }
+}
+
+void sendSOS() {
+  sendS();
+  sendLetterGap();
+  sendO();
+  sendLetterGap();
+  sendS();
+  sendWordGap();
+}
+
+void loop() {
+  sendSOS();
 }
